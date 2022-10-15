793756a88bb194d6e1fb371fc0b093148d9d1212
fjkun06 <fjkun06@gmail.com>
2022-10-15 05:59:55 +0100

    Fixed Missing react-router bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Navbar from './app/Navbar'
-
+import Navbar from "./app/Navbar";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div className="App">
-        <Switch>
-          <Route
+    <>
+      {" "}
+      <Router>
+        <Navbar />
+        <div className="App">
+          {/* <Switch> */}
+          {/* <Route
             exact
             path="/"
-            render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
-            )}
+            render={() => ( */}
+          <section>
+            <h2>Welcome to the Redux Essentials example app!</h2>
+          </section>
+          {/* )}
           />
-          <Redirect to="/" />
-        </Switch>
-      </div>
-    </Router>
-  )
+          <Redirect to="/" /> */}
+          {/* </Switch> */}
+        </div>
+        {" "}
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
